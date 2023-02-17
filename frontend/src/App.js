4a8ec17722f9b06b2c8a03aa890b51dfde613353
HarryLekishon <harry.lekesike@strathmore.edu>
2023-02-17 14:49:40 +0300

    rating component and complete frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,25 @@
 import Footer from "./components/Footer";
 import { Container } from 'react-bootstrap'
 import Header from "./components/Header";
+import HomeScreen from "./screens/HomeScreen";
+import { Routes, Route } from "react-router-dom";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
     <>
+    
       <Header/>
       <main className="py-3">
         <Container>
-            <h1>Welcome to proshop</h1>
+        <Routes>
+            <Route exact path="/" element={ <HomeScreen/>}/>
+            <Route  exact path="/product/:id" element={<ProductScreen/>}/>
+          </Routes>
       </Container>
       </main>
       <Footer/>
+      
     </>
   );
 }
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import { Navbar, Nav, Container } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+
 
 function Header() {
     return (
         <header>
             <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
                 <Container>
-                    <Navbar.Brand href="#home">ProShop</Navbar.Brand>
+                    <Navbar.Brand as={Link} to="/">ProShop</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className='ms-auto'>
-                            <Nav.Link href="#home"><i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
-                            <Nav.Link href="#link"><i className='fas fa-user'></i> Sign In</Nav.Link>
+                            <Nav.Link as={Link} to="/cart"><i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
+                            <Nav.Link as={Link} to="/login"><i className='fas fa-user'></i> Sign In</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
