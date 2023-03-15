df40a32a06c42dd7c91cef1b70b46b1a919460b7
HarryLekishon <harry.lekesike@strathmore.edu>
2023-03-15 12:56:04 +0300

    front and backend joint

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+import Footer from "./components/Footer";
+import { Container } from 'react-bootstrap'
+import Header from "./components/Header";
+import HomeScreen from "./screens/HomeScreen";
+import { Routes, Route } from "react-router-dom";
+import ProductScreen from "./screens/ProductScreen";
+
+function App() {
+  return (
+    <>
+    
+      <Header/>
+      <main className="py-3">
+        <Container>
+        <Routes>
+            <Route exact path="/" element={ <HomeScreen/>}/>
+            <Route  exact path="/product/:id" element={<ProductScreen/>}/>
+          </Routes>
+      </Container>
+      </main>
+      <Footer/>
+     
+      
+    </>
+  );
+}
+
+export default App;
